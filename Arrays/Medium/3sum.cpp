79a343fd7b066315cb13ab3a9a6dfdc6551499a5
tauquirahmed <100079429+tauquirahmed@users.noreply.github.com>
2022-05-21 20:48:41 +0530

    Add files via upload

---
+++ Arrays/Medium/3sum.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+  vector<vector<int>> threeSum(vector<int> &nums)
+  {
+    vector<vector<int>> ans;
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < n - 2; i++)
+    {
+      if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+      {
+        int low = i + 1, high = n - 1, sum = 0 - nums[i];
+        while (low < high)
+        {
+          if (nums[low] + nums[high] == sum)
+          {
+            vector<int> v;
+            v.push_back(nums[i]);
+            v.push_back(nums[low]);
+            v.push_back(nums[high]);
+            ans.push_back(v);
+
+            while (low < high && nums[low] == nums[low + 1])
+              low++;
+            while (low < high && nums[high] == nums[high - 1])
+              high--;
+            low++;
+            high--;
+          }
+          else if (nums[low] + nums[high] < sum)
+            low++;
+          else
+            high--;
+        }
+      }
+    }
+    return ans;
+  }
+};
